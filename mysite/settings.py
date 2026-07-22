3cc91695c99a91e79743926ffbf9f35edf3bb793
Samia <sinan2003bin@gmail.com>
2026-07-22 20:27:05 +0600

    trying

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,6 @@
+import os
+import dj_database_url
+
 """
 Django settings for mysite project.
 
@@ -76,10 +79,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
